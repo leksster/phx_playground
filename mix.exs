3824382097761c7c51295e82b8990d43aedb6e86
Alex Bykov <leksster@gmail.com>
2019-02-28 14:26:54 +0200

    Get rid of warnings

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule PhxPlayground.MixProject do
       aliases: aliases(),
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
