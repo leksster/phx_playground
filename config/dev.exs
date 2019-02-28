a37aa6fd5b7aadaf4fb59b6e803dd73c974cc0db
Alex Bykov <leksster@gmail.com>
2019-02-28 14:49:13 +0200

    Update db config for dev environment

--- config/dev.exs
+++ config/dev.exs
@@ -68,8 +68,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :phx_playground, PhxPlayground.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "aby",
+  password: "",
   database: "phx_playground_dev",
   hostname: "localhost",
   pool_size: 10
